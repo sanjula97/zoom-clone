d33e9b88180d076486746303d9efb0097e831a23
sanjulK <sanjula@hatchyard.io>
2022-01-04 17:41:00 +0530

    project setup

---
+++ components/ContactsMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function ContactsMenu() {
+    return (
+        <View>
+            <Text>ContactsMenu Menu</Text>
+        </View>
+    )
+}
+
+export default ContactsMenu
